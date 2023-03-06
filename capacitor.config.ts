fc9fd34dcd8241f9c2d910178fc76878b3a02334
Sarthak <sarthak.sharma@weebklipper.com>
2023-03-06 13:07:13 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'we-vue-capacitor-sample',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
